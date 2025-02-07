53ad6480f9115644ec126fd6b417a676f08603f2
viennguyen24 <viennguyen@usf.edu>
2025-02-06 23:42:47 -0500

    setup prettier and define rules

---
+++ front-end/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 100,
+  arrowParens: "always",
+  endOfLine: "lf",
+  bracketSpacing: false,
+  proseWrap: "never",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
